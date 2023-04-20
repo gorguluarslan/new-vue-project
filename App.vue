1ea8ebafbc987706b786251c5e0a10e078e5a3a6
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2023-04-20 22:11:06 +0300

    update: add new content

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <CompHeader />
+  <router-view />
+  <CompFooter />
+</template>
+
+<script>
+// @ is an alias to /src
+import CompHeader from "@/components/CompHeader.vue";
+import CompFooter from "@/components/CompFooter.vue";
+export default {
+  name: "HomeView",
+  components: {
+    CompHeader,
+    CompFooter,
+  },
+};
+</script>
