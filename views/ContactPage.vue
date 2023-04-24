29132168e872751f35fe2dc289fe0f9c950ee550
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2023-04-24 17:34:54 +0300

    update: change path

---
+++ views/ContactPage.vue
@@ -0,0 +1,245 @@
+<template>
+  <!-- =======================
+Main banner START -->
+  <section class="pt-4 pt-md-5">
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-xl-12 text-center">
+          <!-- Title -->
+          <h1>Bizimle iletişime geçebilirsiniz</h1>
+          <p class="lead">
+            Hizmetlerimiz hakkında daha detaylı bilgi ve teklif almak için
+            iletişim formumuzu doldurarak istediğiniz zaman bizlerle iletişime
+            geçebilirsiniz.
+          </p>
+        </div>
+      </div>
+
+      <!-- Contact info -->
+      <div class="row g-4 brdr">
+        <!-- Contact item START -->
+        <div class="col-md-6 col-xl-4">
+          <div class="card card-body text-center align-items-center">
+            <!-- Icon -->
+            <img
+              src="../assets/images/icon/phone.png"
+              class="width_icon pb-2"
+            />
+            <!-- Title -->
+            <h5>Call us</h5>
+            <!-- Buttons -->
+            <div class="d-grid gap-3 d-sm-block">
+              <router-link to="/" class="btn btn-sm btn-primary-soft">
+                <i class="bi bi-phone me-2"></i>+123 456 789
+              </router-link>
+              <router-link to="/" class="btn btn-sm btn-light">
+                <i class="bi bi-telephone me-2"></i>+(222)4567 586
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <!-- Contact item END -->
+
+        <!-- Contact item START -->
+        <div class="col-md-6 col-xl-4">
+          <div class="card card-body text-center align-items-center">
+            <!-- Icon -->
+            <img
+              src="../assets/images/icon/email.png"
+              class="width_icon pb-2"
+            />
+            <!-- Title -->
+            <h5>Email us</h5>
+            <!-- Buttons -->
+            <router-link
+              to="/"
+              class="btn btn-link text-decoration-underline p-0 mb-0"
+              ><i class="bi bi-envelope me-1"></i>example@gmail.com</router-link
+            >
+          </div>
+        </div>
+        <!-- Contact item END -->
+
+        <!-- Contact item START -->
+        <div class="col-xl-4 position-relative">
+          <div class="card card-body text-center align-items-center">
+            <!-- Icon -->
+            <img
+              src="../assets/images/icon/world.png"
+              class="width_icon pb-2"
+            />
+            <!-- Title -->
+            <h5>Social media</h5>
+            <!-- Buttons -->
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item">
+                <router-link class="btn btn-sm bg-facebook px-2 mb-0" to="/"
+                  ><i class="bi bi-facebook"></i
+                ></router-link>
+              </li>
+              <li class="list-inline-item">
+                <router-link class="btn btn-sm bg-instagram px-2 mb-0" to="/"
+                  ><i class="bi bi-instagram"></i
+                ></router-link>
+              </li>
+              <li class="list-inline-item">
+                <router-link class="btn btn-sm bg-twitter px-2 mb-0" to="/"
+                  ><i class="bi bi-twitter"></i
+                ></router-link>
+              </li>
+              <li class="list-inline-item">
+                <router-link class="btn btn-sm bg-linkedin px-2 mb-0" to="/"
+                  ><i class="bi bi-linkedin"></i
+                ></router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <!-- Contact item END -->
+      </div>
+    </div>
+  </section>
+  <!-- =======================
+Main banner START -->
+
+  <!-- =======================
+Contact form and vector START -->
+  <section class="pt-0 pt-lg-5">
+    <div class="container">
+      <div class="row g-4 g-lg-5 align-items-center">
+        <!-- Vector image START -->
+        <div class="col-lg-6 text-center">
+          <img src="../assets/images/element/contact.svg" alt="" />
+        </div>
+        <!-- Vector image END -->
+
+        <!-- Contact form START -->
+        <div class="col-lg-6">
+          <div class="card bg-light p-4">
+            <!-- Svg decoration -->
+            <figure class="position-absolute end-0 bottom-0 mb-n4 me-n2">
+              <svg class="fill-orange" width="104.2px" height="95.2px">
+                <circle cx="2.6" cy="92.6" r="2.6"></circle>
+                <circle cx="2.6" cy="77.6" r="2.6"></circle>
+                <circle cx="2.6" cy="62.6" r="2.6"></circle>
+                <circle cx="2.6" cy="47.6" r="2.6"></circle>
+                <circle cx="2.6" cy="32.6" r="2.6"></circle>
+                <circle cx="2.6" cy="17.6" r="2.6"></circle>
+                <circle cx="2.6" cy="2.6" r="2.6"></circle>
+                <circle cx="22.4" cy="92.6" r="2.6"></circle>
+                <circle cx="22.4" cy="77.6" r="2.6"></circle>
+                <circle cx="22.4" cy="62.6" r="2.6"></circle>
+                <circle cx="22.4" cy="47.6" r="2.6"></circle>
+                <circle cx="22.4" cy="32.6" r="2.6"></circle>
+                <circle cx="22.4" cy="17.6" r="2.6"></circle>
+                <circle cx="22.4" cy="2.6" r="2.6"></circle>
+                <circle cx="42.2" cy="92.6" r="2.6"></circle>
+                <circle cx="42.2" cy="77.6" r="2.6"></circle>
+                <circle cx="42.2" cy="62.6" r="2.6"></circle>
+                <circle cx="42.2" cy="47.6" r="2.6"></circle>
+                <circle cx="42.2" cy="32.6" r="2.6"></circle>
+                <circle cx="42.2" cy="17.6" r="2.6"></circle>
+                <circle cx="42.2" cy="2.6" r="2.6"></circle>
+                <circle cx="62" cy="92.6" r="2.6"></circle>
+                <circle cx="62" cy="77.6" r="2.6"></circle>
+                <circle cx="62" cy="62.6" r="2.6"></circle>
+                <circle cx="62" cy="47.6" r="2.6"></circle>
+                <circle cx="62" cy="32.6" r="2.6"></circle>
+                <circle cx="62" cy="17.6" r="2.6"></circle>
+                <circle cx="62" cy="2.6" r="2.6"></circle>
+                <circle cx="81.8" cy="92.6" r="2.6"></circle>
+                <circle cx="81.8" cy="77.6" r="2.6"></circle>
+                <circle cx="81.8" cy="62.6" r="2.6"></circle>
+                <circle cx="81.8" cy="47.6" r="2.6"></circle>
+                <circle cx="81.8" cy="32.6" r="2.6"></circle>
+                <circle cx="81.8" cy="17.6" r="2.6"></circle>
+                <circle cx="81.8" cy="2.6" r="2.6"></circle>
+                <circle cx="101.7" cy="92.6" r="2.6"></circle>
+                <circle cx="101.7" cy="77.6" r="2.6"></circle>
+                <circle cx="101.7" cy="62.6" r="2.6"></circle>
+                <circle cx="101.7" cy="47.6" r="2.6"></circle>
+                <circle cx="101.7" cy="32.6" r="2.6"></circle>
+                <circle cx="101.7" cy="17.6" r="2.6"></circle>
+                <circle cx="101.7" cy="2.6" r="2.6"></circle>
+              </svg>
+            </figure>
+
+            <!-- Card header -->
+            <div class="card-header bg-light p-0 pb-3">
+              <h3 class="mb-0">Send us message</h3>
+            </div>
+
+            <!-- Card body START -->
+            <div class="card-body p-0">
+              <form class="row g-4">
+                <!-- Name -->
+                <div class="col-md-6">
+                  <label class="form-label">Your name *</label>
+                  <input type="text" class="form-control" />
+                </div>
+                <!-- Email -->
+                <div class="col-md-6">
+                  <label class="form-label">Email address *</label>
+                  <input type="email" class="form-control" />
+                </div>
+                <!-- Mobile number -->
+                <div class="col-12">
+                  <label class="form-label">Mobile number *</label>
+                  <input type="text" class="form-control" />
+                </div>
+                <!-- Message -->
+                <div class="col-12">
+                  <label class="form-label">Message *</label>
+                  <textarea class="form-control" rows="3"></textarea>
+                </div>
+                <!-- Checkbox -->
+                <div class="col-12 form-check ms-2">
+                  <input
+                    type="checkbox"
+                    class="form-check-input"
+                    id="exampleCheck1"
+                  />
+                  <label class="form-check-label" for="exampleCheck1">
+                    By submitting this form you agree to our terms and
+                    conditions.
+                  </label>
+                </div>
+                <!-- Button -->
+                <div class="col-12">
+                  <button class="btn btn-dark mb-0" type="button">
+                    Send Message
+                  </button>
+                </div>
+              </form>
+            </div>
+            <!-- Card body END -->
+          </div>
+        </div>
+        <!-- Contact form END -->
+      </div>
+    </div>
+  </section>
+  <!-- =======================
+Contact form and vector END -->
+
+  <!-- =======================
+Map START -->
+  <section class="pt-0 pt-lg-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <iframe
+            class="w-100 h-300px grayscale rounded"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.9663095343008!2d-74.00425878428698!3d40.74076684379132!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259bf5c1654f3%3A0xc80f9cfce5383d5d!2sGoogle!5e0!3m2!1sen!2sin!4v1586000412513!5m2!1sen!2sin"
+            height="500"
+            style="border: 0"
+            aria-hidden="false"
+            tabindex="0"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- =======================
+Map END -->
+</template>
