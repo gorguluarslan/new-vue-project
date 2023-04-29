2ee9631a9566ea79ce5cd3b2b0514d0a1d48052e
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2023-04-29 13:10:27 +0300

    update: add new content

---
+++ components/VillaDetay/CompAciklama.vue
@@ -0,0 +1,75 @@
+<template>
+  <!-- Açıklama -->
+  <section data-aos="fade-down" id="description">
+    <div class="py-3">
+      <div class="card card-body border-light border-2 rounded-6">
+        <div>
+          <h4
+            class="title fw-bold d-flex align-items-center justify-content-center turuncu"
+          >
+            <img
+              src="front/img/detay/aciklama.png"
+              alt=""
+              loading="lazy"
+              style="width: 35px"
+              class="me-2"
+            />Villa Açıklaması
+          </h4>
+          <p>
+            Villa Deluxe 1 Villa Deluxe 1 is modern concept 1 bedroom 2 person
+            private villa with Jacuzzi option located in the Hisaronu area of
+            Fethiye, with a special view of Babadag. Villa Deluxe 1 is 5-10
+            minutes' walk from the center of Hisaronu and only 3.5 km from
+            Ölüdeniz.Modern design , Jacuzzi in the garden and a garden swing
+            offers a comfortable times during your holiday.Villa Deluxe 1 is
+            located in a large complex with 1+1 and 2+1 villas. Note: An extra
+            cleaning fee is required for stays below the minimum length of stay.
+            General Features of Villa Modern Design and Concept Perfect Location
+            1 Bedrooms and Living Area 1 Master Bathrooms Jacuzzi Well-kept
+            Garden Number of people that can stay: 2 people Interior features of
+            our villa; Living Room: There are living room materials suitable for
+            architecture and sitting group, air conditioner, LCD TV, internet,
+            air conditioning and exit to the pool area. Kitchen: Open-plan
+            kitchen area; There are enough kitchen utensils for 2 people and
+            more, dishwasher, oven, stove, refrigerator, teapot and kettle.
+            Terrace: It is the sitting area next to the pool area with tables
+            and chairs. Features of Bedrooms; 1. Bedroom: Master suite bedroom,
+            1 double oval bed, air conditioning, bathroom and wardrobe. External
+            Features; There is a private barbecue, shower area, sun loungers,
+            umbrellas, garden swings. Damage Deposit: A damage deposit of £100
+            is taken during check-in. During your reservation, any broken,
+            dumped etc. If not, the damage deposit will be returned to you.
+            Note: In all our villas, which are intertwined with nature, spraying
+            is done regularly. However, pests etc. in the environment. is likely
+            to be found.The photos of the villas on our site are taken with a
+            'Wide Angle Lens' to fit the image to the screen. For this reason,
+            the objects in the pictures may appear larger or smaller than their
+            actual size. DISTANCES Airport Dalaman Airport, 60 km; It is located
+            within a 50-minute drive. Nearest Beach Oludeniz beach is located
+            3.5 km (5-10 minutes by car or 10 minutes by public transport).
+            Nearest Center Center (Restaurants, Bars, Cafes, Shops, Amusement
+            Park and All Facilities) The center of Hisaronu is 1 km (5-10
+            minutes on foot). Nearest Markets Supermarkets are located 750
+            meters (5-10 minutes walk) from the villa. Fethiye Fethiye center is
+            located 12 km (7-8 minutes by car and 10-15 minutes by public
+            transport). Public transport Minibuses (Dolmus) pass in all
+            directions from the main road, which is 100 meters from the villas.
+          </p>
+          <hr />
+          <div id="readmore">
+            <span class="readmore__content"> </span>
+            <div class="d-flex justify-content-center">
+              <button
+                class="readmore__toggle btn"
+                role="switch"
+                aria-checked="true"
+              >
+                Daha fazla göster
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
