46d5aefd17c50fe865c08703443195fe1300eb46
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2023-04-30 22:33:09 +0300

    update: add new content

---
+++ components/VillaDetay/CompAmenities.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="card mt-4" id="feature">
+    <div class="card-header border-bottom bg-transparent px-0 pt-0">
+      <h3 class="card-title ms-4 mb-0 mt-3">Amenities</h3>
+    </div>
+    <div class="card-body pt-4">
+      <div class="row g-4">
+        <div class="col-sm-6">
+          <h6><i class="fa-solid fa-biking me-2"></i>Activities</h6>
+          <ul class="list-group list-group-borderless mt-2 mb-0">
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Swimming
+              pool
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Spa
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Kids'
+              play area
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Gym
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-sm-6">
+          <h6><i class="fa-solid fa-credit-card me-2"></i>Payment Method</h6>
+          <ul class="list-group list-group-borderless mt-2 mb-0">
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Credit
+              card (Visa, Master card)
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Cash
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Debit
+              Card
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-sm-6">
+          <h6><i class="fa-solid fa-concierge-bell me-2"></i>Services</h6>
+          <ul class="list-group list-group-borderless mt-2 mb-0">
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Dry
+              cleaning
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Room
+              Service
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Special
+              service
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Waiting
+              Area
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Secrete
+              smoking area
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i
+              >Concierge
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Laundry
+              facilities
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Ironing
+              Service
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Lift
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-sm-6">
+          <h6><i class="bi bi-shield-fill-check me-2"></i>Safety & Security</h6>
+          <ul class="list-group list-group-borderless mt-2 mb-4 mb-sm-5">
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Doctor
+              on Call
+            </li>
+          </ul>
+
+          <h6><i class="fa-solid fa-volume-up me-2"></i>Staff Language</h6>
+          <ul class="list-group list-group-borderless mt-2 mb-0">
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>English
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Spanish
+            </li>
+            <li class="list-group-item pb-0">
+              <i class="fa-solid fa-check-circle text-success me-2"></i>Hindi
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
