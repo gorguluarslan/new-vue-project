986efa6e7a7125f6db6e4001725d46f23134255b
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2023-05-04 11:21:37 +0300

    update: add new content

---
+++ components/VillaDetay/CompDonanım.vue
@@ -0,0 +1,60 @@
+<template>
+  <!-- Nav Pills -->
+  <div
+    class="col-12 sticky-top shadow-sm"
+    style="top: 6rem; border-radius: 15px 50px; background-color: #00c9eac7"
+  >
+    <div class="my-4 d-none d-md-block">
+      <div class="villa-nav detay-menu bg-color-1 px-2">
+        <ul
+          class="nav nav-pills1 justify-content-center"
+          id="footer-scroll"
+          role="tablist"
+        >
+          <li class="nav-item" role="presentation">
+            <a
+              class="d-flex align-items-center nav-link1 px-xl-2"
+              href="#prices"
+              ><i class="fas fa-list-ul me-2"></i>Fiyat</a
+            >
+          </li>
+          <li class="nav-item" role="presentation">
+            <a
+              class="d-flex align-items-center nav-link1 px-xl-2"
+              href="#description"
+              ><i class="fas fa-home me-2"></i>Açıklama</a
+            >
+          </li>
+          <li class="nav-item" role="presentation">
+            <a
+              class="d-flex align-items-center nav-link1 px-xl-2"
+              href="#feature"
+              ><i class="fas fa-swimming-pool me-2"></i>Villa Özellikleri</a
+            >
+          </li>
+          <li class="nav-item" role="presentation">
+            <a
+              class="d-flex align-items-center nav-link1 px-xl-2"
+              href="#distances"
+              ><i class="fas fa-paper-plane me-2"></i>Mesafeler</a
+            >
+          </li>
+          <li class="nav-item" role="presentation">
+            <a
+              class="d-flex align-items-center nav-link1 px-xl-2"
+              href="#cm-rules"
+              ><i class="fas fa-calendar-alt me-2"></i>Kurallar</a
+            >
+          </li>
+          <li class="nav-item" role="presentation">
+            <a
+              class="d-flex align-items-center nav-link1 px-xl-2"
+              href="#calendar"
+              ><i class="fas fa-calendar-alt me-2"></i>Takvim</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
