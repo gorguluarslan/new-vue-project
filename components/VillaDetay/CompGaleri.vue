9faa7c4bb3af12ebcb5af6705337efceb4c11508
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2023-05-06 14:27:34 +0300

    update: add new content

---
+++ components/VillaDetay/CompGaleri.vue
@@ -0,0 +1,175 @@
+<template>
+  <section class="card-grid pt-0" style="margin-bottom: -4rem">
+    <div class="container-fluid">
+      <div class="row g-2">
+        <!-- Image -->
+        <div class="col-md-6">
+          <a
+            data-glightbox=""
+            data-gallery="gallery"
+            href="https://booking.webestica.com/assets/images/gallery/14.jpg"
+          >
+            <div
+              class="card card-grid-lg card-element-hover card-overlay-hover overflow-hidden"
+              style="
+                background-image: url(https://booking.webestica.com/assets/images/gallery/14.jpg);
+                background-position: center left;
+                background-size: cover;
+              "
+            >
+              <!-- Card hover element -->
+              <div class="hover-element position-absolute w-100 h-100">
+                <i
+                  class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"
+                ></i>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-md-6 d-sm-none d-md-block d-none d-sm-block">
+          <!-- Card item START -->
+          <div class="row g-2">
+            <!-- Image -->
+            <div class="col-12">
+              <a
+                data-glightbox=""
+                data-gallery="gallery"
+                href="https://booking.webestica.com/assets/images/gallery/14.jpg"
+              >
+                <div
+                  class="card card-grid-sm card-element-hover card-overlay-hover overflow-hidden"
+                  style="
+                    background-image: url(https://booking.webestica.com/assets/images/gallery/14.jpg);
+                    background-position: center left;
+                    background-size: cover;
+                  "
+                >
+                  <!-- Card hover element -->
+                  <div class="hover-element position-absolute w-100 h-100">
+                    <i
+                      class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"
+                    ></i>
+                  </div>
+                </div>
+              </a>
+            </div>
+
+            <!-- Image -->
+            <div class="col-md-6">
+              <a
+                data-glightbox=""
+                data-gallery="gallery"
+                href="https://booking.webestica.com/assets/images/gallery/14.jpg"
+              >
+                <div
+                  class="card card-grid-sm card-element-hover card-overlay-hover overflow-hidden"
+                  style="
+                    background-image: url(https://booking.webestica.com/assets/images/gallery/14.jpg);
+                    background-position: center left;
+                    background-size: cover;
+                  "
+                >
+                  <!-- Card hover element -->
+                  <div class="hover-element position-absolute w-100 h-100">
+                    <i
+                      class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"
+                    ></i>
+                  </div>
+                </div>
+              </a>
+            </div>
+
+            <!-- Images -->
+            <div class="col-md-6">
+              <div
+                class="card card-grid-sm overflow-hidden"
+                style="
+                  background-image: url(https://booking.webestica.com/assets/images/gallery/14.jpg);
+                  background-position: center left;
+                  background-size: cover;
+                "
+              >
+                <!-- Background overlay -->
+                <div class="bg-overlay bg-dark opacity-7"></div>
+
+                <!-- Popup Images -->
+                <a
+                  data-glightbox=""
+                  data-gallery="gallery"
+                  href="https://booking.webestica.com/assets/images/gallery/14.jpg"
+                  class="stretched-link z-index-9"
+                ></a>
+                <a
+                  data-glightbox=""
+                  data-gallery="gallery"
+                  href="https://booking.webestica.com/assets/images/gallery/14.jpg"
+                ></a>
+                <a
+                  data-glightbox=""
+                  data-gallery="gallery"
+                  href="https://booking.webestica.com/assets/images/gallery/14.jpg"
+                ></a>
+
+                <!-- Overlay text -->
+                <div class="card-img-overlay d-flex h-100 w-100">
+                  <h6
+                    class="card-title m-auto fw-light text-decoration-underline"
+                  >
+                    <a href="#" class="text-white">View all</a>
+                  </h6>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- Card item END -->
+        </div>
+        <div class="row pt-3">
+          <div class="fw-bold mb-2 col-6">
+            <i class="bi bi-geo-alt me-2"></i>Fethiye / Ölüdeniz
+          </div>
+          <div
+            class="ls-n05 fs-8 fw-medium d-flex flex-wrap align-items-start col-6 justify-content-end"
+          >
+            <div
+              class="d-flex align-items-center me-1 fs-9"
+              style="font-style: italic"
+            >
+              <img
+                src="../../assets/images/icon/user.gif"
+                width="30"
+                height="21"
+                alt="icon"
+                class="flex-shrink-0 me-1"
+              /><span>4 Kişilik</span>
+            </div>
+            <div
+              class="d-flex align-items-center me-1 fs-9"
+              style="font-style: italic"
+            >
+              <img
+                src="../../assets/images/icon/bed.gif"
+                width="30"
+                height="21"
+                alt="icon"
+                class="flex-shrink-0 me-1"
+              /><span>2 Yatak Odası</span>
+            </div>
+            <div
+              class="d-flex align-items-center me-1 fs-9"
+              style="font-style: italic"
+            >
+              <img
+                src="../../assets/images/icon/bathtub.gif"
+                width="30"
+                height="21"
+                alt="icon"
+                class="flex-shrink-0 me-1"
+              /><span>1 Banyo</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
