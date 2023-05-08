789252e48d6476cd547597baad8911a81f931409
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2023-05-08 22:34:29 +0300

    update: add new content

---
+++ components/VillaDetay/CompKurallar.vue
@@ -0,0 +1,115 @@
+<template>
+  <div
+    class="tab-pane fade active show card mt-3"
+    id="cm-rules"
+    role="tabpanel"
+    aria-labelledby="cm-rules-tab"
+  >
+    <div class="rules-box white-box rounded-lg mb-4 mt-2">
+      <div
+        class="box-header border-bottom border-theme-light-2"
+        style="color: black"
+      >
+        <span
+          class="fw-semibold lh-lg"
+          style="font-size: 30px; display: flex; justify-content: center"
+          >Kurallar</span
+        >
+      </div>
+      <div class="box-content p-4">
+        <ul class="d-flex flex-wrap ps-sm-2 ms-sm-1 pe-xxl-5 pt-sm-2 mb-3">
+          <li class="col-6 d-flex align-items-center mb-4 pb-sm-2">
+            <div
+              class="icon-box flex-shrink-0 icon-50 bg-theme-danger d-flex align-items-center justify-content-center border border-theme-light-2 rounded-circle me-2 me-sm-3"
+            >
+              <img
+                src="https://cdn.supervillam.com/img/spacer.gif"
+                data-src="https://www.supervillam.com/uploads/834_smoking.svg"
+                alt="Sigara İçilmez"
+                width="24"
+                height="24"
+                class="lazy contain loaded"
+                style="
+                  background-image: url('https://www.supervillam.com/uploads/834_smoking.svg');
+                "
+              />
+            </div>
+            <strong class="fs-7 fw-semibold">Sigara İçilmez</strong>
+          </li>
+          <li class="col-6 d-flex align-items-center mb-4 pb-sm-2">
+            <div
+              class="icon-box flex-shrink-0 icon-50 bg-theme-danger d-flex align-items-center justify-content-center border border-theme-light-2 rounded-circle me-2 me-sm-3"
+            >
+              <img
+                src="https://cdn.supervillam.com/img/spacer.gif"
+                data-src="https://www.supervillam.com/uploads/206_track.svg"
+                alt="Evcil Hayvan Giremez"
+                width="24"
+                height="24"
+                class="lazy contain loaded"
+                style="
+                  background-image: url('https://www.supervillam.com/uploads/206_track.svg');
+                "
+              />
+            </div>
+            <strong class="fs-7 fw-semibold">Evcil Hayvan Giremez</strong>
+          </li>
+          <li class="col-6 d-flex align-items-center mb-4 pb-sm-2">
+            <div
+              class="icon-box flex-shrink-0 icon-50 bg-theme-danger d-flex align-items-center justify-content-center border border-theme-light-2 rounded-circle me-2 me-sm-3"
+            >
+              <img
+                src="https://cdn.supervillam.com/img/spacer.gif"
+                data-src="https://www.supervillam.com/uploads/772_confetti.svg"
+                alt="Parti Düzenlenemez"
+                width="24"
+                height="24"
+                class="lazy contain loaded"
+                style="
+                  background-image: url('https://www.supervillam.com/uploads/772_confetti.svg');
+                "
+              />
+            </div>
+            <strong class="fs-7 fw-semibold">Parti Düzenlenemez</strong>
+          </li>
+          <li class="col-6 d-flex align-items-center mb-4 pb-sm-2">
+            <div
+              class="icon-box flex-shrink-0 icon-50 bg-theme-danger d-flex align-items-center justify-content-center border border-theme-light-2 rounded-circle me-2 me-sm-3"
+            >
+              <img
+                src="https://cdn.supervillam.com/img/spacer.gif"
+                data-src="https://www.supervillam.com/uploads/17_little-kid.svg"
+                alt="Çocuklara Uygun (2-12)"
+                width="24"
+                height="24"
+                class="lazy contain loaded"
+                style="
+                  background-image: url('https://www.supervillam.com/uploads/17_little-kid.svg');
+                "
+              />
+            </div>
+            <strong class="fs-7 fw-semibold">Çocuklara Uygun (2-12)</strong>
+          </li>
+          <li class="col-6 d-flex align-items-center mb-4 pb-sm-2">
+            <div
+              class="icon-box flex-shrink-0 icon-50 bg-theme-danger d-flex align-items-center justify-content-center border border-theme-light-2 rounded-circle me-2 me-sm-3"
+            >
+              <img
+                src="https://cdn.supervillam.com/img/spacer.gif"
+                data-src="https://www.supervillam.com/uploads/453_baby.svg"
+                alt="Bebeklere Uygun (0-2)"
+                width="24"
+                height="24"
+                class="lazy contain loaded"
+                style="
+                  background-image: url('https://www.supervillam.com/uploads/453_baby.svg');
+                "
+              />
+            </div>
+            <strong class="fs-7 fw-semibold">Bebeklere Uygun (0-2)</strong>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
