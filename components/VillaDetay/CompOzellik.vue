adf7a23d20fad8ea0e957e02f64edc1dcb3fad8e
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2023-05-10 22:09:26 +0300

    update: add new content

---
+++ components/VillaDetay/CompOzellik.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- Villa Özellikleri -->
+  <section data-aos="fade-down" id="feature">
+    <div class="py-3">
+      <div class="card card-body border-light border-2 rounded-6">
+        <div class="row">
+          <h4
+            class="title fw-bold d-flex align-items-center justify-content-center turuncu"
+          >
+            <img
+              src="front/img/detay/ozellik.png"
+              alt=""
+              loading="lazy"
+              style="width: 35px"
+              class="me-2"
+            />Villa Özellikleri
+          </h4>
+          <hr />
+          <div
+            class="align-items-center d-flex justify-content-between"
+            style="word-wrap: break-word"
+          >
+            <p>Wifi</p>
+            <br />
+            <p>LCD</p>
+            <br />
+            <p>Sıcak Su</p>
+            <br />
+            <p>Havuz</p>
+            <br />
+            <p>Jakuzi</p>
+            <br />
+            <p>Buzdolabı</p>
+            <br />
+            <p>Dondurucu</p>
+            <br />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
