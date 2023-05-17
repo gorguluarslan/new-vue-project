bb90397edc002d913310713487257502cdde7420
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2023-05-17 22:35:45 +0300

    update: add new content

---
+++ components/Anasayfa/CompContent.vue
@@ -0,0 +1,346 @@
+<template>
+  <!-- **************** MAIN CONTENT START **************** -->
+  <main>
+    <!-- =======================
+Client START -->
+    <section class="py-0 py-md-5">
+      <div class="container">
+        <div class="row g-4 g-lg-7 justify-content-center align-items-center">
+          <!-- Image -->
+          <div class="col-5 col-sm-3 col-xl-2">
+            <img src="assets/images/client/01.svg" class="grayscale" alt="" />
+          </div>
+          <!-- Image -->
+          <div class="col-5 col-sm-3 col-xl-2">
+            <img src="assets/images/client/02.svg" class="grayscale" alt="" />
+          </div>
+          <!-- Image -->
+          <div class="col-5 col-sm-3 col-xl-2">
+            <img src="assets/images/client/03.svg" class="grayscale" alt="" />
+          </div>
+          <!-- Image -->
+          <div class="col-5 col-sm-3 col-xl-2">
+            <img src="assets/images/client/04.svg" class="grayscale" alt="" />
+          </div>
+          <!-- Image -->
+          <div class="col-5 col-sm-3 col-xl-2">
+            <img src="assets/images/client/05.svg" class="grayscale" alt="" />
+          </div>
+          <!-- Image -->
+          <div class="col-5 col-sm-3 col-xl-2">
+            <img src="assets/images/client/06.svg" class="grayscale" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- =======================
+Client END -->
+  </main>
+  <!-- **************** MAIN CONTENT END **************** -->
+  <!-- =======================
+Featured Hotels START -->
+  <section>
+    <div class="container">
+      <!-- Title -->
+      <div class="row mb-4">
+        <div class="col-12 text-center">
+          <h2 class="mb-0">Popüler Villalar</h2>
+        </div>
+      </div>
+
+      <div class="row g-4">
+        <!-- Hotel item -->
+        <div class="col-sm-6 col-xl-3">
+          <!-- Card START -->
+          <div class="card card-img-scale overflow-hidden bg-transparent">
+            <!-- Image and overlay -->
+            <div class="card-img-scale-wrapper rounded-3">
+              <!-- Image -->
+              <img
+                src="https://booking.webestica.com/assets/images/category/hotel/04.jpg"
+                class="card-img"
+                alt="villa image"
+              />
+              <!-- Badge -->
+              <div class="position-absolute bottom-0 start-0 p-3">
+                <div
+                  class="badge text-bg-dark fs-6 rounded-pill stretched-link"
+                >
+                  <i class="bi bi-geo-alt me-2"></i>New York
+                </div>
+              </div>
+            </div>
+
+            <!-- Card body -->
+            <div class="card-body px-2">
+              <!-- Title -->
+              <h5 class="card-title">
+                <a href="hotel-detail.html" class="stretched-link"
+                  >Baga Comfort</a
+                >
+              </h5>
+              <!-- Price and rating -->
+              <div class="d-flex justify-content-between align-items-center">
+                <h6 class="text-success mb-0">
+                  $455 <small class="fw-light">/starting at</small>
+                </h6>
+                <h6 class="mb-0">
+                  4.5<i class="fa-solid fa-star text-warning ms-1"></i>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <!-- Card END -->
+        </div>
+
+        <!-- Hotel item -->
+        <div class="col-sm-6 col-xl-3">
+          <!-- Card START -->
+          <div class="card card-img-scale overflow-hidden bg-transparent">
+            <!-- Image and overlay -->
+            <div class="card-img-scale-wrapper rounded-3">
+              <!-- Image -->
+              <img
+                src="https://booking.webestica.com/assets/images/category/hotel/02.jpg"
+                class="card-img"
+                alt="villa image"
+              />
+              <!-- Badge -->
+              <div class="position-absolute bottom-0 start-0 p-3">
+                <div
+                  class="badge text-bg-dark fs-6 rounded-pill stretched-link"
+                >
+                  <i class="bi bi-geo-alt me-2"></i>California
+                </div>
+              </div>
+            </div>
+
+            <!-- Card body -->
+            <div class="card-body px-2">
+              <!-- Title -->
+              <h5 class="card-title">
+                <a href="hotel-detail.html" class="stretched-link"
+                  >New Apollo Hotel</a
+                >
+              </h5>
+              <!-- Price and rating -->
+              <div class="d-flex justify-content-between align-items-center">
+                <h6 class="text-success mb-0">
+                  $585 <small class="fw-light">/starting at</small>
+                </h6>
+                <h6 class="mb-0">
+                  4.8<i class="fa-solid fa-star text-warning ms-1"></i>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <!-- Card END -->
+        </div>
+
+        <!-- Hotel item -->
+        <div class="col-sm-6 col-xl-3">
+          <!-- Card START -->
+          <div class="card card-img-scale overflow-hidden bg-transparent">
+            <!-- Image and overlay -->
+            <div class="card-img-scale-wrapper rounded-3">
+              <!-- Image -->
+              <img
+                src="https://booking.webestica.com/assets/images/category/hotel/03.jpg"
+                class="card-img"
+                alt="villa image"
+              />
+              <!-- Badge -->
+              <div class="position-absolute bottom-0 start-0 p-3">
+                <div
+                  class="badge text-bg-dark fs-6 rounded-pill stretched-link"
+                >
+                  <i class="bi bi-geo-alt me-2"></i>Los Angeles
+                </div>
+              </div>
+            </div>
+
+            <!-- Card body -->
+            <div class="card-body px-2">
+              <!-- Title -->
+              <h5 class="card-title">
+                <a href="hotel-detail.html" class="stretched-link"
+                  >New Age Hotel</a
+                >
+              </h5>
+              <!-- Price and rating -->
+              <div class="d-flex justify-content-between align-items-center">
+                <h6 class="text-success mb-0">
+                  $385 <small class="fw-light">/starting at</small>
+                </h6>
+                <h6 class="mb-0">
+                  4.6<i class="fa-solid fa-star text-warning ms-1"></i>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <!-- Card END -->
+        </div>
+
+        <!-- Hotel item -->
+        <div class="col-sm-6 col-xl-3">
+          <!-- Card START -->
+          <div class="card card-img-scale overflow-hidden bg-transparent">
+            <!-- Image and overlay -->
+            <div class="card-img-scale-wrapper rounded-3">
+              <!-- Image -->
+              <img
+                src="https://booking.webestica.com/assets/images/category/hotel/04.jpg"
+                class="card-img"
+                alt="villa image"
+              />
+              <!-- Badge -->
+              <div class="position-absolute bottom-0 start-0 p-3">
+                <div
+                  class="badge text-bg-dark fs-6 rounded-pill stretched-link"
+                >
+                  <i class="bi bi-geo-alt me-2"></i>Chicago
+                </div>
+              </div>
+            </div>
+
+            <!-- Card body -->
+            <div class="card-body px-2">
+              <!-- Title -->
+              <h5 class="card-title">
+                <a href="hotel-detail.html" class="stretched-link"
+                  >Helios Beach Resort</a
+                >
+              </h5>
+              <!-- Price and rating -->
+              <div class="d-flex justify-content-between align-items-center">
+                <h6 class="text-success mb-0">
+                  $665 <small class="fw-light">/starting at</small>
+                </h6>
+                <h6 class="mb-0">
+                  4.8<i class="fa-solid fa-star text-warning ms-1"></i>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <!-- Card END -->
+        </div>
+      </div>
+      <!-- Row END -->
+    </div>
+  </section>
+  <!-- =======================
+Featured Hotels END -->
+
+  <!-- =======================
+Yorum START -->
+
+  <!-- =======================
+Yorum END -->
+
+  <section class="short-terms-section mb-5 pb-3 bg-orange">
+    <div class="container bg-gradient brdr">
+      <div class="short-term bg-theme-first text-white rounded p-4 p-md-5">
+        <div class="row py-lg-2 py-3xl-4 gx-0 px-3xl-5">
+          <div
+            class="col-lg-4 st-caption d-flex flex-column pe-md-4 pe-3xl-5 pb-3 py-lg-3"
+          >
+            <strong class="fs-4 fw-medium lh-sm d-block mb-3">
+              Kısa Süreli Kiralıklara Gözatın
+            </strong>
+            <p class="description text-secondary1 ls-05 mb-0">
+              Tarihler arasında boş kalan ara tarihlere göz atın
+            </p>
+          </div>
+          <div
+            class="col-lg-8 col-3xl-6 d-flex align-items-center justify-content-end ms-auto"
+          >
+            <form
+              action="arama-sonuclari"
+              method="get"
+              class="st-engine fw-medium flex-shrink-0 d-flex flex-wrap flex-md-nowrap w-100"
+            >
+              <input type="hidden" name="kisasureli" value="1" />
+              <div
+                class="bg-gradient st-item flex-fill bg-theme-first-light rounded d-flex align-items-center py-2 px-4 me-2 mb-md-0 mb-2"
+              >
+                <div class="d-flex flex-column flex-fill ps-3xl-3 p-md-1">
+                  <span class="ms-1 text-secondary1">Yıl</span
+                  ><select
+                    name="yil"
+                    id=""
+                    class="bg-transparent p-0 text-white fs-6 border-0 pe-2 pe-lg-4"
+                  >
+                    <option value="2024" style="color: #111">2024</option>
+                    <option value="2023" selected="" style="color: #111">
+                      2023
+                    </option>
+                  </select>
+                </div>
+                <i
+                  class="icon-arrow-triangle-down text-secondary fs-8 mt-3"
+                ></i>
+              </div>
+              <div
+                class="bg-gradient st-item flex-fill bg-theme-first-light rounded d-flex align-items-center py-2 px-4 me-md-2 ms-lg-1 mb-md-0 mb-2"
+              >
+                <div class="d-flex flex-column flex-fill ps-3xl-3 p-md-1">
+                  <span class="ms-1 text-secondary1">Ay</span
+                  ><select
+                    name="ay"
+                    id=""
+                    class="bg-transparent p-0 text-white fs-6 border-0 pe-2 pe-lg-4"
+                  >
+                    <option value="1" style="color: #111">Ocak</option>
+                    <option value="2" style="color: #111">Şubat</option>
+                    <option value="3" style="color: #111">Mart</option>
+                    <option value="4" selected="selected" style="color: #111">
+                      Nisan
+                    </option>
+                    <option value="5" style="color: #111">Mayıs</option>
+                    <option value="6" style="color: #111">Haziran</option>
+                    <option value="7" style="color: #111">Temmuz</option>
+                    <option value="8" style="color: #111">Ağustos</option>
+                    <option value="9" style="color: #111">Eylül</option>
+                    <option value="10" style="color: #111">Ekim</option>
+                    <option value="11" style="color: #111">Kasım</option>
+                    <option value="12" style="color: #111">Aralık</option>
+                  </select>
+                </div>
+                <i
+                  class="icon-arrow-triangle-down text-secondary fs-8 mt-3"
+                ></i>
+              </div>
+              <div
+                class="bg-gradient st-item flex-fill bg-theme-first-light rounded d-flex align-items-center py-2 px-4 me-2 ms-lg-1"
+              >
+                <div class="d-flex flex-column flex-fill ps-3xl-3 p-md-1">
+                  <span class="ms-1 text-secondary1">Gece</span
+                  ><select
+                    name="gece"
+                    id=""
+                    class="bg-transparent p-0 text-white fs-6 border-0 pe-2 pe-lg-4"
+                  >
+                    <option value="3" style="color: #111">3 Gece</option>
+                    <option value="4" style="color: #111">4 Gece</option>
+                    <option value="5" style="color: #111">5 Gece</option>
+                    <option value="6" style="color: #111">6 Gece</option>
+                    <option value="7" style="color: #111">7 Gece</option>
+                  </select>
+                </div>
+                <i
+                  class="icon-arrow-triangle-down text-secondary fs-8 mt-3"
+                ></i>
+              </div>
+              <button
+                type="submit"
+                class="bg-gradient st-item btn btn-theme-second text-white shadow-none fs-7 fw-medium rounded flex-shrink-0 p-3 p-md-4"
+              >
+                <span class="d-inline-block px-3xl-2 m-3xl-1">Ara</span>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
