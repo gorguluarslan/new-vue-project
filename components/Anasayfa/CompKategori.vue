38c1ed306814c38ad381002c4259633d81c89062
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2023-05-19 23:47:59 +0300

    update: add new content

---
+++ components/Anasayfa/CompKategori.vue
@@ -0,0 +1,113 @@
+<template>
+  <section class="pt-5 pt-md-8 pb-0">
+    <div class="container-fluid px-lg-5">
+      <!-- Title -->
+
+      <div class="row mb-4">
+        <div class="col-12 text-center">
+          <h2>Offers Of The Month</h2>
+        </div>
+      </div>
+
+      <div class="row g-4">
+        <!-- Offer item -->
+        <div class="col-6 col-md-4 col-xl-2">
+          <div class="card bg-transparent h-100">
+            <img
+              src="https://booking.webestica.com/assets/images/offer/07.jpg"
+              class="card-img"
+              alt=""
+            />
+            <div class="card-body text-center p-2">
+              <h6 class="mb-0">
+                <a href="#" class="stretched-link">Pass Holder Package</a>
+              </h6>
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer item -->
+        <div class="col-6 col-md-4 col-xl-2">
+          <div class="card bg-transparent h-100">
+            <img
+              src="https://booking.webestica.com/assets/images/offer/07.jpg"
+              class="card-img"
+              alt=""
+            />
+            <div class="card-body text-center p-2">
+              <h6 class="mb-0">
+                <a href="#" class="stretched-link">More Sun, More Fun</a>
+              </h6>
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer item -->
+        <div class="col-6 col-md-4 col-xl-2">
+          <div class="card bg-transparent h-100">
+            <img
+              src="https://booking.webestica.com/assets/images/offer/07.jpg"
+              class="card-img"
+              alt=""
+            />
+            <div class="card-body text-center p-2">
+              <h6 class="mb-0">
+                <a href="#" class="stretched-link">A Stay That Gives Back</a>
+              </h6>
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer item -->
+        <div class="col-6 col-md-4 col-xl-2">
+          <div class="card bg-transparent h-100">
+            <img
+              src="https://booking.webestica.com/assets/images/offer/07.jpg"
+              class="card-img"
+              alt=""
+            />
+            <div class="card-body text-center p-2">
+              <h6 class="mb-0">
+                <a href="#" class="stretched-link">Elevate Your Stay</a>
+              </h6>
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer item -->
+        <div class="col-6 col-md-4 col-xl-2">
+          <div class="card bg-transparent h-100">
+            <img
+              src="https://booking.webestica.com/assets/images/offer/07.jpg"
+              class="card-img"
+              alt=""
+            />
+            <div class="card-body text-center p-2">
+              <h6 class="mb-0">
+                <a href="#" class="stretched-link">Spa Package Offer</a>
+              </h6>
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer item -->
+        <div class="col-6 col-md-4 col-xl-2">
+          <div class="card bg-transparent h-100">
+            <div class="bg-light card-img position-relative h-100">
+              <button
+                class="btn btn-lg btn-primary btn-round mb-0 position-absolute top-50 start-50 translate-middle"
+              >
+                <i class="bi bi-arrow-right"></i>
+              </button>
+            </div>
+            <div class="card-body text-center p-2">
+              <h6 class="mb-0">
+                <a href="#" class="stretched-link">View All Offers</a>
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
